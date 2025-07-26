eeb38997e56f8928fb234f98a874af347859bb85
wkdkavishka <w.k.dumindukavishka@gmail.com>
2025-07-27 01:30:50 +0530

    reorganize footer

--- src/components/FooterComp.tsx
+++ src/components/FooterComp.tsx
@@ -8,12 +8,12 @@ export const Footer = () => {
     return (
         <footer className="rounded-4xl border-1 border-teal-200 bg-teal-100/50 p-2 shadow-lg backdrop-blur">
             <div className="mx-auto max-w-4xl text-center">
-                <p className="text-gray-600 dark:text-gray-400">
+                <p className="text-teal-600 dark:text-teal-400">
                     © {currentYear} {personal.name}. All rights reserved.
                 </p>
-                <p className="mt-2 text-sm text-teal-600 dark:text-teal-400">
+                {/* <p className="mt-2 text-sm text-teal-600 dark:text-teal-400">
                     Built with Next.js, TypeScript, and Tailwind CSS
-                </p>
+                </p> */}
             </div>
         </footer>
     );
