20ec1c286d1901eaea44536c7a928c7a0618e21a
wkdkavishka <w.k.dumindukavishka@gmail.com>
2025-09-02 00:36:33 +0530

    cache ttl is moved to _headder for static site

--- src/types/speed-insights.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@vercel/speed-insights/next' {
-    import React from 'react';
-
-    interface SpeedInsightsProps {
-        route?: string;
-    }
-
-    export const SpeedInsights: React.FC<SpeedInsightsProps>;
-}
