55e5daaf8c4269985f7955ee7de037b53ede647e
wkdkavishka <w.k.dumindukavishka@gmail.com>
2025-09-30 23:14:57 +0530

    testing serwist

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import withSerwistInit from '@serwist/next';
+
+const nextConfig = {
+    output: 'export', // This is crucial for static export
+    images: {
+        unoptimized: true, // Required for static export
+    },
+    distDir: 'dist',
+};
+
+const withSerwist = withSerwistInit({
+    // use something else that works, such as "service-worker/index.ts".
+    swSrc: 'src/app/sw.ts',
+    swDest: 'public/sw.js',
+    cacheOnNavigation: true,
+    reloadOnOnline: true,
+    disable: process.env.NODE_ENV === 'development', // Optional: disable in dev
+});
+
+export default withSerwist(nextConfig);
